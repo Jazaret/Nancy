6e55156427c45bb644c612eaf99d6335c3430ffd
Javier Azaret <jazaret@gmail.com>
2018-07-10 11:13:44 -0400

    add moq to NancyTest

--- NancyApplication.Tests/ModuleTests.cs
+++ NancyApplication.Tests/ModuleTests.cs
@@ -3,25 +3,34 @@ using Xunit;
 using NancyApplication;
 using Nancy;
 using Nancy.Testing;
+using Moq;
 
 namespace NancyApplication.Tests
 {
     public class TopicModuleTests
     {   
+        private readonly Mock<ITopicRepository> _mockTopicRepo;
+        private readonly Mock<ITopicService> _mockTopicService;
+        public TopicModuleTests() {
+            _mockTopicRepo = new Mock<ITopicRepository>(MockBehavior.Loose);
+            _mockTopicService = new Mock<ITopicService>(MockBehavior.Loose);
+        }
         [Fact]
         public void AssertRouteExists()
         {
-            // // Given
-            // var bootstrapper = new DefaultNancyBootstrapper();
-            // var browser = new Browser(bootstrapper);
+            // Given
+            var browser = new Browser((c) => c.Module<TopicsModule>()
+                .Dependency<ITopicRepository>(_mockTopicRepo.Object)
+                .Dependency<ITopicService>(_mockTopicService.Object)
+            );
             
-            // // When
-            // var result = browser.Get("/", with => {
-            //     with.HttpRequest();
-            // });
+            // When
+            var result = browser.Get("/", with => {
+                with.HttpRequest();
+            });
                 
-            // // Then
-            // Assert.Equal(HttpStatusCode.OK, result.Result.StatusCode);
+            // Then
+            Assert.Equal(HttpStatusCode.OK, result.Result.StatusCode);
         }   
     } 
 }
